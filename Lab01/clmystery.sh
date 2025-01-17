0d7e3d705d496884cdc1d77ac89b655ccf9250bb
Matusita <85855107+PedroMatusita@users.noreply.github.com>
2025-01-17 01:24:09 +0000

    Lab1

---
+++ Lab01/clmystery.sh
@@ -0,0 +1,90 @@
+ls
+cd Laboratorios/lab01/
+ls
+cd clmystery-pt-BR-v2/
+ls
+cat inicio
+cd misterio/
+ls
+cat cena-do-crime 
+grep PISTA cena-do-crime 
+cd ..
+ls
+cat inicio
+cd misterio/
+ls
+cat pessoas
+head pessoas 
+head -n 50 pessoas 
+grep Annabel pessoas 
+ls
+cd ruas
+ls
+cat Vila_Buckingham 
+head -n 179 Vila_Buckingham 
+head -n 40 Vila_Hart 
+ls
+cd ..
+ls
+cd entrevistas/
+ls
+cat entrevista-699607
+cat entrevista-47246024
+cd ..
+ls
+cat automoveis 
+grep "Placa L337" automoveis 
+grep "Placa L337ZR9" automoveis 
+grep -A 10 "Placa L337ZR9" automoveis 
+grep -A 10 "Placa L337P89" automoveis 
+grep "Placa L337" automoveis | grep "Marca: Honda"
+grep "Placa L337" automoveis | grep "Marca: Honda" automoveis 
+grep "Placa L337" automoveis | grep "Marca: Honda"
+grep -A 10 "Placa L337" automoveis 
+grep -A 6 "Placa L337" automoveis 
+ls
+cat pessoas
+grape "Erika Owens" pessoas 
+grep "Erika Owens" pessoas 
+grep "Joe Germuska" pessoas 
+grep "Rienne Lemeda" pessoas 
+grep "Jacqui Maher" pessoas 
+ls
+cd associacoes/
+ls
+grep Germuska Delta_SkyMiles 
+grep "Rienne Lemeda" Delta_SkyMiles 
+grep "Rienne Lemeda" AAA
+grep "Rienne Lemeda" Biblioteca_da_Cidade 
+grep "Rienne Lemeda" Museu_da_História_do_Bash 
+cd ..
+ls
+cd ruas
+ls
+head -n 284 Travessa_Dunstable 
+cd ..
+ls
+cd entrevistas/
+ls
+cat entrevista-9620713
+cd ..
+cd ruas
+head -n 275 Rua_Plainfield 
+cd ..
+cd entrevistas/
+cat entrevista-29741223 
+cd ..
+ls
+cd ..
+ls
+cat inicio 
+echo "Culpado: Rienne Lemeda" > culpado.txt
+ls
+cat solucao 
+echo " " && sed -n '/Z/p' solucao | tail -n 6 | cut -c 8,22,23,41 && echo " "
+Rienne Lemeda
+echo "Rienne Lemeda"
+history -w ./clmystery.sh
+ls
+cat clmystery.sh 
+Rienne Lemeda
